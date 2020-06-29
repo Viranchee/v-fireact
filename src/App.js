a2d44ba3820359f83c45982b00441b46a6b31ee5
Viranchee Lotia <viranchee@outlook.com>
2020-06-29 13:37:30 +0530

    Add Prettier and ESLint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 // Basic HTML Components
-import Header from "./Components/Header";
+import Header from './Components/Header'
 // Context
-import { AuthProvider } from "./Auth"
+import { AuthProvider } from './Auth'
 // Views
-import Home from "./Views/Home";
-import Login from "./Views/Login";
-import Signup from "./Views/Signup";
-import PrivateRoute from "./PrivateRoute";
+import Home from './Views/Home'
+import Login from './Views/Login'
+import Signup from './Views/Signup'
+import PrivateRoute from './PrivateRoute'
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         </React.Fragment>
       </Router>
     </AuthProvider>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Views/Login.js
+++ src/Views/Login.js
@@ -1,20 +1,18 @@
 import React, { useCallback, useContext } from "react";
-import { withRouter, Redirect } from "react-router"
-import { AuthContext } from "../Auth"
+import { withRouter, Redirect } from "react-router";
+import { AuthContext } from "../Auth";
 import { signIn } from "../Firebase/auth";
 
 function Login({ history }) {
-
-  const handleLogin = useCallback(async event => {
-    const { email, password } = event.target.elements
+  const handleLogin = useCallback(async (event) => {
+    const { email, password } = event.target.elements;
 
     console.log(email, password);
     await signIn(email.value, password.value)
       .then(() => alert("Success"))
-      .then(() => history.push('/'))
-      .catch((error) => alert(error))
-
-  })
+      .then(() => history.push("/"))
+      .catch((error) => alert(error));
+  });
 
   // const handleLogin1 = useCallback(async event => {
   //   event.preventDefault()
@@ -28,32 +26,42 @@ function Login({ history }) {
   //   }
   // }, [history])
 
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
-  if (currentUser) { return <Redirect to="/" /> }
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <React.Fragment>
       <h5>Hey, please DO NOT use your real Email address and Password.</h5>
-      <h6>Since these email password aren't verified, use throwaway data here</h6>
+      <h6>
+        Since these email password aren't verified, use throwaway data here
+      </h6>
       <h2>Log In</h2>
       <form onSubmit={handleLogin}>
-        <label>Email
+        <label>
+          Email
           <input name="email" type="email" placeholder="Email" />
         </label>
         <br />
-        <label>Password
+        <label>
+          Password
           <input name="password" type="password" placeholder="Password" />
         </label>
         <br />
         <input type="checkbox" name="savePwd" id="" />
-        <label > Save Password</label>
+        <label> Save Password</label>
         <br />
         <input type="submit" value="Login" />
-        <input type="button" value="Sign Up" onClick={() => history.push("/signup")} />
+        <input
+          type="button"
+          value="Sign Up"
+          onClick={() => history.push("/signup")}
+        />
       </form>
-    </React.Fragment >
+    </React.Fragment>
   );
 }
 
-export default withRouter(Login)
+export default withRouter(Login);
